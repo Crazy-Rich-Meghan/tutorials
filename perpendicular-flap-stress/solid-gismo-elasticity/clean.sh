fe319e69855f2c18a2e330b0bfa305d35e54fa2e
Crazy-Rich-Meghan <delft2021li@gmail.com>
2024-12-04 16:00:18 +0100

    Update clean.sh

--- perpendicular-flap-stress/solid-gismo-elasticity/clean.sh
+++ perpendicular-flap-stress/solid-gismo-elasticity/clean.sh
@@ -1,7 +1,25 @@
-#!/bin/sh
-set -e -u
+#!/bin/bash
 
-. ../../tools/cleaning-tools.sh
+# Cleaning script for solid-gismo-elasticity directory
 
-clean_gismo .
+echo "Cleaning unnecessary files..."
 
+# Remove precice-profiling directory
+if [ -d "precice-profiling" ]; then
+    rm -rf precice-profiling
+    echo "Deleted 'precice-profiling' folder."
+fi
+
+# Remove precice-run directory
+if [ -d "../precice-run" ]; then
+    rm -rf ../precice-run
+    echo "Deleted 'precice-run' folder."
+fi
+
+# Remove files ending with .pvd, .vts, .vtp, .log, and .txt
+for ext in pvd vts vtp log txt; do
+    find . -type f -name "*.$ext" -exec rm -f {} \;
+    echo "Deleted all *.$ext files."
+done
+
+echo "Cleaning completed!"
