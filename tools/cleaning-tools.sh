0a56df72754df6389681f84eff5c8fecb7823791
Benjamin Uekermann <benjamin.uekermann@ipvs.uni-stuttgart.de>
2025-01-22 16:53:31 +0100

    Extend and enable cleaning scripts

--- tools/cleaning-tools.sh
+++ tools/cleaning-tools.sh
@@ -189,25 +189,27 @@ clean_fmi() {
 
 clean_matlab(){
     (
-	set -e -u
-	cd "$1"
-	echo "- Cleaning up MATLAB case in $(pwd)"
-	clean_precice_logs .
-	clean_case_logs .
+        set -e -u
+        cd "$1"
+        echo "- Cleaning up MATLAB case in $(pwd)"
+        clean_precice_logs .
+        clean_case_logs .
     )	
 }
 clean_gismo(){
     (
-    set -e -u
-	cd "$1"
-	echo "- Cleaning up G+Smo case in $(pwd)"
-    for ext in pvd vts vtp log txt; do
-        for file in **/*."$ext"; do
-            # Check if the file exists
-            [ -f "$file" ] && rm -f "$file"
+        set -e -u
+        cd "$1"
+        echo "- Cleaning up G+Smo case in $(pwd)"
+        for ext in pvd vts vtp log txt; do
+            for file in **/*."$ext"; do
+                # Check if the file exists
+                [ -f "$file" ] && rm -f "$file"
+            done
         done
-    done
-    clean_precice_logs .
-    clean_case_logs .
+        rm -fv pointData.csv
+        rm -rfv ./output/
+        clean_precice_logs .
+        clean_case_logs .
     )
 }
